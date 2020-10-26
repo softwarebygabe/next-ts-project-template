ac3f2fd8b1c594ee700116f7706e1228091a72b1
Gabe Szczepanek <gabe@blend.com>
2020-10-26 11:40:20 -0400

    finish the next demo

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const name = 'Gabe Szczepanek'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: { children?: any, home?: boolean }) {
   return (
     <div className={styles.container}>
       <Head>
