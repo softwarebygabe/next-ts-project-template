aae6aedd0bc4411b5024cbffeb38fa07b8d1e382
Gabe Szczepanek <gabe@blend.com>
2020-10-26 12:20:21 -0400

    refactor and comments

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,31 @@
 import express, { Request, Response } from 'express'
 import next from 'next'
-import { mainModule } from 'process'
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
 
 async function createApp() {
-  const app = next({ dev })
-  const handle = app.getRequestHandler()
-  await app.prepare()
+  // create and prepare the next app
+  const nextApp = next({ dev })
+  await nextApp.prepare()
 
   const server = express()
 
+  // create and attach backend routes here
   server.get('/api/data', (req, res) => {
     console.log(`Request received at ${req.path}`)
+    const fakeLoadingTime = 2000 // ms
     setTimeout(() => {
       res.send({
         location: 'New York City',
         position: 'Software Engineer',
       })
-    }, 2000)
+    }, fakeLoadingTime)
   })
 
+  // handle all other requests with the next app's handler
   server.all('*', (req: Request, res: Response) => {
+    const handle = nextApp.getRequestHandler()
     return handle(req, res)
   })
 
